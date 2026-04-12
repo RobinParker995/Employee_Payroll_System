ba90de251b7beec12c89a6f35b3b8afc72b3609c
RobinParker995 <robinparker995@gmail.com>
2026-04-12 20:01:35 +0530

    Fixed the Code

--- employeePayrollSystem.c
+++ employeePayrollSystem.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
 
-struct Employee {
+struct Employee
+{
     int id;
     char name[50];
     float basicSalary;
@@ -11,47 +14,55 @@ struct Employee {
     float netSalary;
 };
 
-int main() {
+int main()
+{
     int n, i;
+    char buffer[100];
 
-    printf("Enter number of employees:\n ");
-    scanf("%d", &n);
+    printf("Enter number of employees: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    n = (int)strtol(buffer, NULL, 10);
 
     struct Employee emp[n];
 
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("\n--- Enter details for Employee %d ---\n", i + 1);
 
-        printf("Enter ID:\n ");
-        scanf("%d", &emp[i].id);
+        // ID
+        printf("Enter ID: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        emp[i].id = (int)strtol(buffer, NULL, 10);
 
-        printf("Enter Name:\n ");
-        scanf("%s", emp[i].name);
+        // Name
+        printf("Enter Name: ");
+        fgets(emp[i].name, sizeof(emp[i].name), stdin);
 
-        printf("Enter Basic Salary:\n ");
-        scanf("%f", &emp[i].basicSalary);
+        // Remove newline from name
+        emp[i].name[strcspn(emp[i].name, "\n")] = '\0';
 
-        // Allowances
-        emp[i].hra = 0.20 * emp[i].basicSalary; // 20% HRA
-        emp[i].da  = 0.10 * emp[i].basicSalary; // 10% DA
+        // Basic Salary
+        printf("Enter Basic Salary: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        emp[i].basicSalary = strtof(buffer, NULL);
 
-        // Gross Salary
+        // Calculations
+        emp[i].hra = 0.20 * emp[i].basicSalary;
+        emp[i].da = 0.10 * emp[i].basicSalary;
         emp[i].grossSalary = emp[i].basicSalary + emp[i].hra + emp[i].da;
 
-        // Tax Calculation
-        if(emp[i].grossSalary > 50000)
-            emp[i].tax = 0.10 * emp[i].grossSalary; // 10% tax
+        if (emp[i].grossSalary > 50000)
+            emp[i].tax = 0.10 * emp[i].grossSalary;
         else
-            emp[i].tax = 0.05 * emp[i].grossSalary; // 5% tax
+            emp[i].tax = 0.05 * emp[i].grossSalary;
 
-        // Net Salary
         emp[i].netSalary = emp[i].grossSalary - emp[i].tax;
     }
 
-    // Display Results
     printf("\n\n===== Employee Payroll Details =====\n");
 
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("\nEmployee %d\n", i + 1);
         printf("ID: %d\n", emp[i].id);
         printf("Name: %s\n", emp[i].name);
